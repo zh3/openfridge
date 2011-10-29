c04e51799a32577350762852e7a9569676cf36ef
Jesse Weinstein <jesse@wefu.org>
2011-10-29 02:48:29 -0700

    Switch to using my fractional layout system, since I can't get the scrol to work... We'll discuss on Sunday (or later today?)

--- OpenFridge_ZH/src/com/openfridge/ExpirationListActivity.java
+++ OpenFridge_ZH/src/com/openfridge/ExpirationListActivity.java
@@ -7,13 +7,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
 public class ExpirationListActivity extends Activity {
-	//Tag for debug log
+	// Tag for debug log
 	private static final String DEBUG_TAG = "Openfridge";
 
 	/** Called when the activity is first created. */
@@ -23,43 +25,53 @@ public class ExpirationListActivity extends Activity {
 
 		Log.d(DEBUG_TAG, "checkpoint");
 
-		//This code gets the xml and sets up the SAX Parser
+		// This code gets the xml and sets up the SAX Parser
 		FridgeFoodDataClient client = new FridgeFoodDataClient();
 		try {
 			client.reloadFoods();
 
-		setContentView(R.layout.expiration_list);
-			//Setup the Listview
-			
-			initFridgeFoodListView(R.id.pastLV, client.getPastFoods(), 
-			        new PastFridgeItemClickListener());
-			initFridgeFoodListView(R.id.nearLV, client.getPastFoods(), 
-                    new PastFridgeItemClickListener());
-			initFridgeFoodListView(R.id.goodLV, client.getPastFoods(), 
-                    new PastFridgeItemClickListener());
+			setContentView(R.layout.expiration_list);
+			// Setup the Listview
+
+			initFridgeFoodListView(R.id.pastLV, client.getPastFoods(),
+					new PastFridgeItemClickListener());
+			initFridgeFoodListView(R.id.nearLV, client.getPastFoods(),
+					new PastFridgeItemClickListener());
+			initFridgeFoodListView(R.id.goodLV, client.getPastFoods(),
+					new PastFridgeItemClickListener());
 		} catch (Exception e) {
 			// For debugging
 			e.printStackTrace();
 		}
 
 	}
-public void removeItem(View view) {
-	 Toast.makeText(this, "remove clicked!", 
-				Toast.LENGTH_SHORT).show();
-}
-	private void initFridgeFoodListView(int viewId, ArrayList<FridgeFood> foods,
-	        OnItemClickListener listener) {
-	    ListView listView = (ListView) findViewById(viewId);
-        listView.setChoiceMode(2);   //CHOICE_MODE_MULTIPLE
-        listView.setTextFilterEnabled(true);
-        listView.setAdapter(new ArrayAdapter<FridgeFood>(this,
-        		R.layout.expiration_list_item, R.id.text, foods));
-        listView.setOnItemClickListener(listener);
-    
+
+	public void removeItem(View view) {
+		Toast.makeText(this, "remove clicked!", Toast.LENGTH_SHORT).show();
 	}
-	
+
+	private void initFridgeFoodListView(int viewId,
+			ArrayList<FridgeFood> foods, OnItemClickListener listener) {
+		ListView listView = (ListView) findViewById(viewId);
+		listView.setTextFilterEnabled(true);
+		listView.setAdapter(new ArrayAdapter<FridgeFood>(this,
+				R.layout.expiration_list_item, R.id.text, foods));
+		//listView.setOnItemClickListener(listener);
+		
+        ListAdapter listAdapter = listView.getAdapter();
+
+        int rows = listAdapter.getCount();
+        int height = android.R.attr.listPreferredItemHeight * rows;
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = height;
+        listView.setLayoutParams(params);
+        listView.requestLayout();
+
+	}
+
 	public void loadItemEdit(View view) {
-	    Intent intent = new Intent(this, ItemEditActivity.class);
-        startActivity(intent);
+		Intent intent = new Intent(this, ItemEditActivity.class);
+		startActivity(intent);
 	}
 }
